1d4eaa9b0c0874a6fc93719fee06b72f1569d94a
MusaabShalaldeh <musaabsh65@yahoo.com>
2021-11-15 19:01:30 +0200

    feat: added functionality and ability to traverse between snack pages

--- snack_tracker_project/snacks/views.py
+++ snack_tracker_project/snacks/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from .models import Snack
 
 
 class SnackListView(ListView):
     template_name = "snack_list.html"
+    model = Snack
+
+class SnackDetailView(DetailView):
+    template_name = "snack_detail.html"
+    model = Snack
+    context_object_name = "snack_object"
